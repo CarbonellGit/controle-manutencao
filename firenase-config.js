2937475095284ef8ba2cf8712c03d024488e7812
CarbonellGit <ti.github@colegiocarbonell.com.br>
2025-08-01 16:31:29 -0300

    Create firenase-config.js

---
+++ firenase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2MF68rcGVi1KtZDhfkiuHqFFpnI8PILI",
+    authDomain: "controle-equipamentos-4d988.firebaseapp.com",
+    projectId: "controle-equipamentos-4d988",
+    storageBucket: "controle-equipamentos-4d988.appspot.com",
+    messagingSenderId: "837758315044",
+    appId: "1:837758315044:web:fe0b453754e845518f06b1"
+};
